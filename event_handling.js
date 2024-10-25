740a032f914693dce059dd21040d40f5aa98a2ec
= <=>
2024-10-25 16:49:16 -0400

    Calculate and update total price dynamically based on product and quantity

--- event_handling.js
+++ event_handling.js
@@ -1,20 +1,25 @@
+// Setting up constants 
+
 const product = document.getElementById('product');
 const quantity = document.getElementById('quantity');
 const totalPrice = document.getElementById('price');
 const submit = document.getElementById('submit');
 const orderSummary = document.getElementById('order-summary');
 
-//Task 2: Add Event Listener for Product Selection 
-function calculateTotalPrice() {
-    const productPrice = parseFloat(product.value);
+//Task 2: Add Event Listener for Product Selection
+
+function calculateTotalPrice() { //Using function to calculate total price
+    const productPrice = parseFloat(product.value); 
     const quantityValue = parseInt(quantity.value);
     const price = productPrice * quantityValue;
     
-    totalPrice.textContent = `Total Price: $${price.toFixed(2)}`
+    totalPrice.textContent = `Total Price: $${price.toFixed(2)}` //Display price on html page
 }
 
-product.addEventListener('change', calculateTotalPrice);
-quantity.addEventListener('input', calculateTotalPrice);
+// Task 3: Calculate Total Price Dynamically
+
+product.addEventListener('change', calculateTotalPrice); // Using eventListener to change price when different options from dropdown menu are selected
+quantity.addEventListener('input', calculateTotalPrice); // Using eventListener to change price when different quantity inputs are entered
 
 // // Handle order submission
 // placeOrderButton.addEventListener('click', function() {
